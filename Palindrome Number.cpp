82ed2ab09cce6291afbf52243b12a1ae17fbb7bb
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-26 12:42:52 +0530

    Update Palindrome Number.cpp

--- Palindrome Number.cpp
+++ Palindrome Number.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int k=x;
+        long reverse=0;
+        int lastdigit=0;
+        while(x>0)
+        {
+            lastdigit=x%10;
+            reverse=reverse*10 + lastdigit;
+            x=x/10;
+        }
+        
+        return((k==reverse)? true: false);
+        
+    }
+};
