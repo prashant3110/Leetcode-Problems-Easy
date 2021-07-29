a3b0f48a6b15e4dadb4aa46b44423e4007f46672
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-29 20:28:08 +0530

    Update Fibonacci Number.cpp

--- Fibonacci Number.cpp
+++ Fibonacci Number.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+public:
+    int fib(int n) {
+        if(n<2){
+            return n;
+        }
+        
+        int a[n+1];
+        int i;
+        a[0]=0;
+        a[1]=1;
+        
+        for(i=2;i<=n;i++)
+        {
+            a[i]=a[i-1]+a[i-2];
+              
+        }
+        
+       return a[n];
+        
+      
+        
+    }    
+        
+    
+};
+    
