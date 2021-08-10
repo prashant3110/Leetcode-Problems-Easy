42a3b2faefa0d23fb59919d1fc1aee23089a4a39
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-10 17:46:52 +0530

    final commit

--- Two Sum II - Input array is sorted.cpp
+++ Two Sum II - Input array is sorted.cpp
@@ -13,11 +13,10 @@ Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
 
 
 Example 2:
-
 Input: numbers = [2,3,4], target = 6
 Output: [1,3]
-Example 3:
 
+Example 3:
 Input: numbers = [-1,0], target = -1
 Output: [1,2]
  
