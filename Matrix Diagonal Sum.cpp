a5954a1d3316cebbd77335297905b799a72e3ba7
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-21 13:12:17 +0530

    Update Matrix Diagonal Sum.cpp

--- Matrix Diagonal Sum.cpp
+++ Matrix Diagonal Sum.cpp
@@ -1,1 +1,24 @@
+// Given a square matrix mat, return the sum of the matrix diagonals.
 
+//Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        int sum=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if((i==j) || (i+j==n-1))
+                {
+                    sum+=mat[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+};
