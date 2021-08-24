3faa032ebd26cf0e39a4e1e774cd0476f256fe7a
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-24 12:02:04 +0530

    Update Matrix Diagonal Sum.cpp

--- Matrix Diagonal Sum.cpp
+++ Matrix Diagonal Sum.cpp
@@ -8,7 +8,7 @@ public:
         int m=mat[0].size();
         int sum=0;
         
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++) 
         {
             for(int j=0;j<m;j++)
             {
