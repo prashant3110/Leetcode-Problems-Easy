d957b2e7c4045f560d88de46aee1ecc67222a4ad
Prashant <53570660+prashant3110@users.noreply.github.com>
2022-01-17 18:38:00 +0530

    Update Length of Last Word.cpp

--- Length of Last Word.cpp
+++ Length of Last Word.cpp
@@ -21,4 +21,29 @@ Explanation: The last word is "joyboy" with length 6.
 
 */
 
-
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int l= s.length();
+        int count=0;
+          for(int i=l-1;i>=0;i--)
+          {
+              if(s[i]==' '&& count>0)
+              {
+                  break;
+              }
+              else if(s[i]==' '&& count==0)
+              {
+                continue;  
+              }
+              else
+              {
+                  count++;
+              }
+          }
+            
+        
+        
+        return count;
+    }
+};
