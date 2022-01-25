6dc61be830995a9a99781d763ce736176825e521
Prashant <53570660+prashant3110@users.noreply.github.com>
2022-01-25 13:19:42 +0530

    Update Search Insert Position.cpp

--- Search Insert Position.cpp
+++ Search Insert Position.cpp
@@ -17,3 +17,34 @@ Input: nums = [1,3,5,6], target = 7
 Output: 4
 
 */
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n=nums.size();
+        int l=0;
+        int h=n;
+        int index;
+        while(l<=h)
+        {
+           int mid=(l+h)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>target)
+            {
+                h=mid-1;
+             }
+            else 
+            {
+                l=mid+1;
+                
+            }
+              if(l == nums.size()) return l;
+        }
+        
+     return l;
+       
+    }
+};
